9fc4c4afb836dafa82b4a7e52308d4f9b4e85e03
roger chat <mattrogerpelosi@gmail.com>
2021-09-21 17:46:01 -0400

    commit tree 4b4fa03e09a911328f263c27a16e646e40e48aaf

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
 
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title) 
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+        self.genre = genre
+    end 
+
+        def turn_page
+            puts "Flipping the page...wow, you read fast!"
+        end 
+
+end
 
